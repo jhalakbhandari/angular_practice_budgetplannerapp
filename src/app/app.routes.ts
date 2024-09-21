89bcfe698b82969d8fed3ca0ee1c7bec82a8570e
jhalakbhandari <82494331+jhalakbhandari@users.noreply.github.com>
2024-09-21 07:40:04 +0530

    project route commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'budget-planner', // Redirect to 'budget-planner' by default
+    pathMatch: 'full',
+  },
   {
     path: 'budget-planner',
     loadChildren: () =>
@@ -8,4 +14,12 @@ export const routes: Routes = [
         (m) => m.BudgetPlannerModule
       ),
   },
+  // Optionally add a wildcard route to catch unknown routes
+  { path: '**', redirectTo: 'budget-planner/login' }, // Catch-all for undefined routes
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
